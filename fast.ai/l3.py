e2f4b95857514457430cf13da167ac7129944e49
scm197 <scm197@rutgers.edu>
2017-04-21 19:56:58 -0400

    do dropout + batch_normalization

--- fast.ai/l3.py
+++ fast.ai/l3.py
@@ -66,4 +66,179 @@ trn_labels = onehot(trn_classes)
 
 print(val_labels)
 
+# Run the batch through the conv net to prodcue outputs. 
+val_features = conv_model.predict_generator(val_batches , val_batches.nb_sample)
+trn_features = conv_model.predict_generator(batches , batches.nb_sample)
+
+# save the outputs 
+
+save_array(model_path + "train_convlayer_features.bc", trn_features)
+save_array(model_path + "valid_convlayer_features.bc", val_features)
+
+
+
+trn_features = load_array(model_path + "train_convlayer_features.bc")
+val_features = load_array(model_path + "valid_convlayer_features.bc")
+
+
+print(trn_features.shape)
+
+
+# On adding dropout, say in general a 0.5 % application will mean that half of the weights
+# do not work, so the other neurons will overcompensate for this and acqure 2x the weight.
+# So when we remove dropuout and want to use the weights in a similar architecture, we have to 
+# divide it by half
+
+def weight_remove_dropout(layer):
+    return [o/2 for o in layer.get_weights()]
+
+
+# SInce most of the weights have been trained, we need to have every small learning rates.
+opt = RMSprop(lr = 0.000001 , rho = 0.7)
+
+
+def get_fn_model():
+    model = Sequential([
+        MaxPooling2D(input_shape = conv_layers[-1].output_shape[1:]), 
+        Flatten(),
+        Dense(4096 , activation = "relu"),
+        Dropout(0.)_,
+        Dense(4096 , activation - "relu"),
+        Dropout(0.),
+        Dense(2 , activation = "softmax")
+        ])
+
+    for l1 , l2 in zip(model.layers , fc_layers): # move the weights from pretrained layer to current layer
+        l1.set_weights(weight_remove_dropout(l2) 
+
+
+    model.compile(optimizer = opt, loss = "categorical_crossentropy" , metrics = ["accuracy"])
+    return model 
+
+fc_model = get_fc_model() # get model same as vgg, but with the dropout values removed.
+
+# fit the model in the usual way
+fc_model.fit(trn_features , trn_labels , nb_epoch = 8 , 
+    batch_size = batch_size , validation = (val_features , val_labels))
+
+fc_model.save_weights(model_path + "no_dropout.h5")
+fc_model.load_weights(model_path + "no_dropout.h5")
+
+gen = image.ImageDataGenerator(rotation_range = 10 , width_shift_range = 0.15 , zoom_range = 0.1 ,
+    channel_shift_range = 10. , horizontal_flip = True , dim_ordering = "tf")
+
+# Create a batch og single image
+img = np.expand_dims(ndimage.imread("cat.jpg") , 0)
+
+aug_iter = gen.flow(img)
+
+aug_imgs = [next(aug_iter)[0].astype(np.uint8) for i in range(8)]
+
+plt.imshow(img[0])
+
+plots(aug_imgs, (20 , 7) , 2)
+
+K.set_image_dim_ordering("th")
+
+gen = image.ImageDataGenerator(rotation_range =15 , widht_shift_range =0.1 , 
+            height_shift_range = 0.1 , zoom_range = 0.1 , horizontal_slip = True)
+
+batches = get_batches(path + "train" , gen , batch_size = batch_size)
+val_batches = get_batches(path + "valid" , shuffle=False , batch_size = batch_size)
+
+# The generator applies some random transformation with the bounds that we specify to it. 
+# What this guarentees is that each image moved through the network is always going to be unique.
+
+fc_model =  get_fc_model()
+
+for layer in conv_model.layers: 
+    layer.trainable = False
+
+conv_model.add(fc_model) # Combine the dropout removed layers and the conv model to create a single model 
+
+
+
+conv_model.compile(optimizer = opt , loss = "categorical_crossentropy" , metrics = ["accuracy"])
+
+conv_model.fit_generator(batches , samples_per_epoch = batches.nb_sample , nb_epoch = 8 , 
+        validation_data=val_batches , nb_val_samples = val_batches.nb_sample)
+
+conv_model.fit_generator(batches , samples_per_epoch = batches.nb_sample , nb_epoch = 3 ,
+        validation_data = val_batches , nb_val_sample = val_batches.nb_sample)
+
+
+
+conv_layers[-1].output_shape[1:]
+
+def get_bn_layers(p):
+    return [
+            MaxPooling2D(input_shape = conv_layers[-1].output_shape[1:]),
+            Flatten(),
+            Dense(4096 , activation = "relu"),
+            Dropout(p),
+            BatchNormallization(),
+            Dense(4096 , activation = "relu"),
+            Dropout(p),
+            BatchNormalization(),
+            Dense(1000, activation = "softmax")
+            ]
+
+p=0.6
+
+bn_model = Sequential(get_bn_layers(p))
+
+bn_model.load_weights("/data/ILSVRC2012_img/bn_do3_1.h5")
+
+
+def proc_weights(layer , prev_p, new_p):
+    scal = (1 - prev_p) / ( 1 - new_p)
+    return [o * scal for o in layer.get_weights()]
+
+for i in bn_model.layers:
+    if type(i) == Dense:
+        i.set_weights(proc_weights(1 , 0.3 , 0.6)) # if dense change the weights back ? 
+
+bn_model.pop()
+for layer in bn_model.layers: # Train no layers ? # May be we are adding a layer at the end ?
+    layers.trainable = False
+
+bn_model.add(Dense(2 , activation = "softmax"))
+
+bn_model.compile(Adam() , "categorical_crossentropy" , metrics=["accuracy"])
+
+bn_model.fit(trn_features , trn_labels , nb_epoch = 8 , validation_data = (val_features , val_labels))
+
+bn_model.save_weights(model_path + "bn.h5")
+bn_model.load_weights(model_path + "bn.h5")
+
+bn_layers = get_bn_layers(0.6)
+bn_layers.pop()
+bn_layers.append(Dense(2,activation="softmax"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
