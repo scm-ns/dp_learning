e1167c4e0d9770914a7a3ec9a962f0943b340100
scm197 <scm197@rutgers.edu>
2017-05-01 14:50:39 -0400

    simple typing errors

--- tf_src/tf_layers_tut.py
+++ tf_src/tf_layers_tut.py
@@ -7,10 +7,9 @@ import numpy as np
 import tensorflow as tf
 
 from tensorflow.contrib import learn
-from tensroflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
+from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 tf.logging.set_verbosity(tf.logging.INFO)
-
 
 def cnn_model_fn(features, labels, mode):
     input_layer = tf.reshape(features , [-1 , 28 , 28 , 1])
@@ -32,7 +31,7 @@ def cnn_model_fn(features, labels, mode):
      
     dense = tf.layers.dense(inputs =pool2_flat , units = 1024 , activation = tf.nn.relu)
 
-    dropout = tf.layers.droput(inputs = dense , rate = 0.4 , training=mode == learn.ModeKeys.Train)
+    dropout = tf.layers.dropout(inputs = dense , rate = 0.4 , training=mode == learn.ModeKeys.TRAIN)
 
     logits = tf.layers.dense(inputs= dropout , units = 10)
     loss = None
@@ -43,18 +42,16 @@ def cnn_model_fn(features, labels, mode):
         loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels , logits = logits)
 
     if mode  == learn.ModeKeys.TRAIN:
-        train_op = tf.contrib.layers.optimize_loss(uloss = goss , global_step =tf.contrib.framework.get_global_step(),learning_rate = 0.001 , optimizer="SGD")
+        train_op = tf.contrib.layers.optimize_loss(loss = loss , global_step =tf.contrib.framework.get_global_step(),learning_rate = 0.001 , optimizer="SGD")
 
 
     predictions = {
             "classes" : tf.argmax(input = logits,axis = 1),
             "probabilities" : tf.nn.softmax(logits , name="softmax_tensor")
     }
-
-
     return model_fn_lib.ModelFnOps(mode = mode , predictions = predictions , loss = loss , train_op = train_op)
 
-def main():
+def main(no_arg):
     mnist = learn.datasets.load_dataset("mnist")
 
     train_data = mnist.train.images
@@ -65,12 +62,11 @@ def main():
 
     mnist_classifier = learn.Estimator(model_fn = cnn_model_fn , model_dir = "/tmp/mnist_convnet_model")
 
-
     # Proper way to set up logging
-    tensors_to_log = {"probabilities" : "softax_tensor"}
+    tensors_to_log = {"probabilities" : "softmax_tensor"}
     logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log , every_n_iter = 50)
 
-    mnist_classifier.fit(x = train.data , y = train.labels , batch_size = 100 , steps = 20000,
+    mnist_classifier.fit(x = train_data , y = train_labels , batch_size = 100 , steps = 20000,
             monitors = [logging_hook])
 
     metrics = {"accuracy" : learn.MetricSpec(metric_fn =tf.metrics.accuracy , prediction_key = "classes")}
@@ -78,8 +74,6 @@ def main():
     eval_results = mnist_classifier.evaluate(x = eval_data , y=eval_labels , metrics = metrics)
 
     print(eval_results)
-
-
 
 if __name__ == "__main__" :
     tf.app.run()
