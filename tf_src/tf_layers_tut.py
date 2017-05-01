311fd97025105fe5a50cee0054eb76e02a3bfad0
scm197 <scm197@rutgers.edu>
2017-05-01 13:15:00 -0400

    tf layers update simple

--- tf_src/tf_layers_tut.py
+++ tf_src/tf_layers_tut.py
@@ -18,7 +18,7 @@ def cnn_model_fn(features, labels, mode):
     conv1 = tf.layers.conv2d(inputs = input_layer , filters = 32 , kernel_size = [5 , 5] , padding ="same" , 
             activation=tf.nn.relu)
 
-    pool1 = tf.layers.max_pooling2d(inputs = conv1 , pool_sizw = [2 , 2] , strides = 2)
+    pool1 = tf.layers.max_pooling2d(inputs = conv1 , pool_size = [2 , 2] , strides = 2)
 
     # Convolutional layer #2 
     conv2 = tf.layers.conv2d(inputs = pool1 , filters = 64 , kernel_size = [5,5] , padding="same" , 
@@ -37,6 +37,24 @@ def cnn_model_fn(features, labels, mode):
     logits = tf.layers.dense(inputs= dropout , units = 10)
     loss = None
     train_op = None
+
+    if mode != learn.ModeKeys.INFER:
+        onehot_labels = tf.one_hot(indices = tf.cast(labels, tf.int32) , depth = 10)
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels , logits = logits)
+
+    if mode  == learn.ModeKeys.TRAIN:
+        train_op = tf.contrib.layers.optimize_loss(uloss = goss , global_step =tf.contrib.framework.get_global_step(),learning_rate = 0.001 , optimizer="SGD")
+
+
+    predictions = {
+            "classes" : tf.argmax(input = logits,axis = 1),
+            "probabilities" : tf.nn.softmax(logits , name="softmax_tensor")
+    }
+
+
+    return model_fn_lib.ModelFnOps(mode = mode , predictions = predictions , loss = loss , train_op = train_op)
+
+
 
 
 if __name__ == "__main__" :
