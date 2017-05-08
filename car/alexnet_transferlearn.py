5a632ba4f5ae544eacb8ad5ce59e0e14745aa530
scm197 <scm197@rutgers.edu>
2017-05-08 18:35:39 -0400

    set up the base for transfer learning; import alexnet classes

--- car/alexnet_transferlearn.py
+++ car/alexnet_transferlearn.py
@@ -1,8 +1,19 @@
 import numpy as np
+import tensorflow as tf
 
 train_x =  np.zeros((1, 277 , 227 , 3),dtype=np.float)
-print(train_x)
 train_y = np.zeros((1,1000),dtype=np.int)
+xdim = train_x.shape[1:]
+ydim = train_y.shape[1:]
 
+from alexnet import AlexNet
+from caffe_classes import class_names
+
+x = tf.placeholder(tf.float32 ,(None,) + xdim)
+resized = tf.image.resize_images(x , (227,227))
+
+
+
+prob = 
 
 
