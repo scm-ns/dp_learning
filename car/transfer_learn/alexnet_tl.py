903e8f5c9c988c2c09ec5075b41eecd31223ffdc
scm197 <scm197@rutgers.edu>
2017-05-11 17:53:38 -0400

    changes to use custom alex net with custom last layer

--- car/transfer_learn/alexnet_tl.py
+++ car/transfer_learn/alexnet_tl.py
@@ -9,11 +9,12 @@ from alexnet import AlexNet
 from caffe_classes import class_names
 
 input_img_dim = (32 , 32 , 3)
+custom_layer_layer_classes = 43
 
 x = tf.placeholder(tf.float32 ,(None,) + input_img_dim)
 resized = tf.image.resize_images(x , (227,227))
 
-prob = alex_net(resized)
+prob = alex_net(resized , custom_layer_layer_classes)
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
@@ -36,7 +37,7 @@ for input_im_index in range(output.shape[0]):
     indexs = np.argsort(output)[input_im_index,:] # arrange in increasing order
     print("Image : " , input_im_index)
     for i in range(5): # print the top 5 classes, indexs , the most probable classes are towards the end
-        print("%s: %.3f" % (class_names[indexs[-1-i]], output[input_im_index , indexs[-1-i]])) 
+        print("%s: %.3f" % (indexs[-1-i], output[input_im_index , indexs[-1-i]])) 
     print()
 
 print("Time :%.3f seconds" % (time.time() - t))
