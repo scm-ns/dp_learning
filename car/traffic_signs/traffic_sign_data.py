fbdef2a2a7d7d7683eaca8384e87b20843058da5
scm197 <scm197@rutgers.edu>
2017-05-11 19:25:07 -0400

    basic machinary to read the data set

--- car/traffic_signs/traffic_sign_data.py
+++ car/traffic_signs/traffic_sign_data.py
@@ -3,4 +3,20 @@ from sklearn.model_selection import train_test_split
 from keras.utils import np_utils
 import csv 
 
+train_data_file = "./data/train.p"
+test_data_file = "./data/test.p"
+
+with open(train_data_file , mode ="rb") as f:
+    train = pickle.load(f)
+with open(test_data_file  , mode ="rb") as f:
+    test = pickle.load(f)
+
+x_train , y_train = train["features"] , train["labels"]
+y_test , y_test =   test["features"] , test["labels"]
+
+with open("signnames.csv", "r") as csvfile:
+    reader = csv.reader(csvfile)
+    print(reader)
+
+    
 
