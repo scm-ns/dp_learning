f109f71b9f5b54ccc02bd4141be0f3ca75a99edb
scm197 <scm197@rutgers.edu>
2017-05-11 19:39:56 -0400

    modify scipt to change file names from *.p to *.pkl

--- car/traffic_signs/get_data.sh
+++ car/traffic_signs/get_data.sh
@@ -1,3 +1,14 @@
-wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d53ce_traffic-sign-data/traffic-sign-data.zip
+wget https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d53ce_traffic-sign-data/traffic-sign-data.zip 
 unzip *.zip
 rm *.zip
+# the zip has data in a lab* folder, move it to data
+mv lab* data
+cd data
+rm *docx
+
+# Enter data and rename *.p files in *.pkl files
+for file in *.p
+do 
+	mv "$file" "${file%.p}.pkl"
+done
+
